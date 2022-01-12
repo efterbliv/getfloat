8d24327d9fdff505ada7de98080d93308cbf22d2
efterbliv <97608667+efterbliv@users.noreply.github.com>
2022-01-12 14:32:36 +0100

    Create getfloat.go

---
+++ getfloat.go
@@ -0,0 +1,27 @@
+package getfloat
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return number, nil
+}
